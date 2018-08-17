22c229f6b3e1771ce9fd1f9c706cdb8b233c05c8
sebastian fuenza rails <sebastian.fuenza@gmail.com>
2018-08-17 15:09:45 -0300

    CRUD sobre USER ADMIN

---
+++ app/admin/users.rb
@@ -0,0 +1,54 @@
+ActiveAdmin.register User do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+permit_params :name, :email, :password
+
+index do
+  column :id
+  column :name
+  column :email
+  column :sign_in_count
+  column :last_sign_in_at
+  column :provider
+  column :member_since do |user|
+    time_ago_in_words(user.created_at)
+  end
+  actions
+
+
+end
+
+form do |f|
+  inputs 'Agregando un nuevo usuario' do
+    input :name
+    input :email
+    input :password
+
+  end
+  actions
+
+end
+
+controller do
+  def update
+    if (params[:user][:password].blank? && params[:user][:password_confirmation].blank?)
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+    end
+    super
+  end
+end
+
+
+end
