041aa8d49c5b11b8714f92bb22e57de560df6c11
sebastian fuenza rails <sebastian.fuenza@gmail.com>
2018-08-24 15:47:35 -0300

    belongs to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :ideas, dependent: :destroy
+  has_many :idea_likes, dependent: :destroy
 end
